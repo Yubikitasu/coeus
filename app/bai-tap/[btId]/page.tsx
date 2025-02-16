fb49e42ccafed894813d44ac0546affb5aff25c1
Khoa Phạm <76861131+Yubikitasu@users.noreply.github.com>
2025-02-16 07:36:33 +0000

    .

--- app/bai-tap/[btId]/page.tsx
+++ app/bai-tap/[btId]/page.tsx
@@ -1,5 +1,6 @@
 import RenderCauHoi from "@/components/pages/CauHoi";
 import CauHoiFormTN from "@/components/pages/CauHoiFormTN";
+import Navbar from "@/components/pages/NavBar";
 
 export default async function Page({
   params,
@@ -8,9 +9,15 @@ export default async function Page({
 }) {
   const btId = (await params).btId;
   return (
-    <div className="flex flex-col items-center min-h-screen form p-6 space-y-2" id={btId}>
-      <CauHoiFormTN btId={btId} />
-      <RenderCauHoi baiTapId={btId}></RenderCauHoi>
-    </div>
+    <>
+      <Navbar />
+      <div
+        className="flex flex-col items-center min-h-screen form p-6 space-y-2"
+        id={btId}
+      >
+        <CauHoiFormTN btId={btId} />
+        <RenderCauHoi baiTapId={btId}></RenderCauHoi>
+      </div>
+    </>
   );
 }
